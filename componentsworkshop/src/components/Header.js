019904e14567660f7ed8cc541cdd8717062860f6
Madlena Ivanova <madlena.iv@abv.bg>
2023-06-23 23:01:36 +0300

    routing

--- componentsworkshop/src/components/Header.js
+++ componentsworkshop/src/components/Header.js
@@ -1,4 +1,5 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
 
@@ -11,16 +12,16 @@ const Header = () => {
         <header>
         <h1><NavLink className="home" to="/">GamesPlay</NavLink></h1>
         <nav>
-            <NavLink activeStyle={activeLinkStyles} to="/games">All games</NavLink>
+            <NavLink activeClassName="active-navigation" to="/games">All games</NavLink>
 
             <div id="user">
-                <NavLink activeStyle={activeLinkStyles} to="/create-game">Create Game</NavLink>
-                <NavLink activeStyle={activeLinkStyles} to="logout">Logout</NavLink>
+                <NavLink activeClassName="active-navigation" to="/create-game">Create Game</NavLink>
+                <NavLink activeClassName="active-navigation" to="logout">Logout</NavLink>
             </div>
 
             <div id="guest">
-                <NavLink activeStyle={activeLinkStyles} to="login">Login</NavLink>
-                <NavLink activeStyle={activeLinkStyles} to="register">Register</NavLink>
+                <NavLink activeClassName="active-navigation" to="login">Login</NavLink>
+                <NavLink activeClassName="active-navigation" to="register">Register</NavLink>
             </div>
         </nav>
     </header>
