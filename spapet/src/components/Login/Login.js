eba6aa7179c781d8fb9c97e6731c759454dad7ff
Madlena Ivanova <madlena.iv@abv.bg>
2023-07-21 20:39:40 +0300

    spa

--- spapet/src/components/Login/Login.js
+++ spapet/src/components/Login/Login.js
@@ -6,10 +6,6 @@ const Login = () => {
         let formData = new FormData(e.currentTarget);
 
         let email = formData.get('email');
-        let email = formData.get('email');
-        let email = formData.get('email');
-        let email = formData.get('email');
-        let email = formData.get('email');
     }
 
     return(
@@ -32,7 +28,25 @@ const Login = () => {
                 <input className="button submit" type="submit" value="Login" />
             </fieldset>
         </form>
-    </section>
+    <form id="login-form" onSubmit={onLogin} >
+        <fieldset>
+            <legend>Login Form</legend>
+            <p className="field">
+                <label htmlFor="email">Email</label>
+                <span className="input">
+                    <input type="text" name="email" id="email" placeholder="Email" />
+                </span>
+            </p>
+            <p className="field">
+                <label htmlFor="password">Password</label>
+                <span className="input">
+                    <input type="password" name="password" id="password" placeholder="Password" />
+                </span>
+            </p>
+            <input className="button submit" type="submit" value="Login" />
+        </fieldset>
+    </form>
+</section>
     )
 }
 
