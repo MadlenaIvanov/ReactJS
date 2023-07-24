b087c56a587fee259ebd55bb5f2fef5b0daf2722
Madlena Ivanova <madlena.iv@abv.bg>
2023-07-24 22:54:52 +0300

    spa

--- spapet/src/components/Login/Login.js
+++ spapet/src/components/Login/Login.js
@@ -6,8 +6,6 @@ const Login = () => {
         let formData = new FormData(e.currentTarget);
 
         let email = formData.get('email');
-        let email = formData.get('email');
-        let email = formData.get('email');
     }
 
     return(
