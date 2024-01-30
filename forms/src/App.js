ea93dc27f887bbd5b6e3aac7b2ef5b41de515f3d
Madlena Ivanova <madlena.iv@abv.bg>
2024-01-30 21:47:25 +0200

    drop menu added

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,20 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
-  const [isValid, setIsValid] = useState(false)
+  const [services, setServices] = useState([]);
+  const [isValid, setIsValid] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:3030/jsonstore/services')
+    .then(res => res.json())
+    .then(res => {
+      setServices(Object.values(res));
+    })
+  }, []);
 
 
   const submitHandler = (e) => {
     e.preventDefault();
 
     let formData = new FormData(e.currentTarget)
-    let { username, password, isAdmin } = Object.fromEntries(formData)
+    let { username, password, isAdmin, services } = Object.fromEntries(formData)
 
     console.log(username)
     console.log(password)
     console.log(Boolean(isAdmin))
+    console.log(services)
   }
 
   const onChange = (e) => {
@@ -45,9 +55,19 @@ function App() {
         </div>
 
         <div>
+           <label htmlFor='services'>Services</label>
+          <select name='services' id='services'>
+            {services.map(x => <option key={x._id} value={x._id}>{x.name}</option>)}
+          </select>
+        </div>
+
+        <div>
            <label htmlFor='isAdmin'>Is Admin</label>
           <input type="checkBox" name='isAdmin' id='isAdmin' defaultChecked />
         </div>
+
+
+
         <input type="submit" value='Login' />
       </form>
     </div>
